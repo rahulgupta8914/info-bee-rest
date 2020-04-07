def53b954bd123bbca74fc697f4ec1120618f241
Rahul Gupta <gupta8914@gmail.com>
2020-04-07 17:22:31 +0530

    patch cors update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, x-authorization, Content-Type, Accept"
